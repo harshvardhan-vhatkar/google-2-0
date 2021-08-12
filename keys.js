f2fe29f60f7dfb33cb85b4f1873ef1527859acbd
Harshvardhan Vhatkar <79767191+iknowyoupeople@users.noreply.github.com>
2021-08-13 02:20:32 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
 //api key - developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyBWozW1duyRy3qRYBq4cvtplaHu8ZKK0zw'
+require("dotenv").config();
+
+export const API_KEY = process.env.APIS_KEY;
 //context key- cse.google.com/cse/create/new
-export const CONTEXT_KEY = 'bb5453a102ab9c7b2'
+export const CONTEXT_KEY = process.env.CONTEXTS_KEY;
