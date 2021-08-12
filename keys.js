2ed29b8759857d499f7fbb4d49d50c176a104dea
Harshvardhan Vhatkar <harshuvhatkar@gmail.com>
2021-08-13 00:59:38 +0530

    app build

---
+++ keys.js
@@ -0,0 +1,4 @@
+//api key - developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyBWozW1duyRy3qRYBq4cvtplaHu8ZKK0zw'
+//context key- cse.google.com/cse/create/new
+export const CONTEXT_KEY = 'bb5453a102ab9c7b2'
